fcd920fb89890f2e0d448ad763b35e21c7f7514f
hongfaqiu <951142905@qq.com>
2023-02-24 15:25:21 +0800

    fix(demo): init add one layer to avoid repeat add

--- example/src/components/Earth/index.tsx
+++ example/src/components/Earth/index.tsx
@@ -49,12 +49,6 @@ const EarthViewer: React.FC<ViewerProps> = ({
             mode: 'hslLong'
           }
         }
-      })
-      await addLayer({
-        layerName: 'landsat',
-        id: '2',
-        method: 'cog',
-        url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
       }, {
         zoom: true
       })
